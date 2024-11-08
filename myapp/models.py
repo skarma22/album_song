3d65b71a0820e37e9213bb068dbbce629f160e84
skarma22 <skarma615@gmail.com>
2024-11-08 12:45:22 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,1 @@
 from django.db import models
-
-
-class SongData(models.Model):
-    main_title = models.CharField(max_length=255)
-    lyrics_content = models.TextField()
-    expert = models.CharField(max_length=255)
-    featuring_detail = models.CharField(max_length=255)
-    album = models.CharField(max_length=255)
-    release_date = models.CharField(max_length=255)
-    genius_link = models.URLField()
